835306209175339dbf26e1e7802813dedfae38a9
Christopher Atwell <catwell@mys3tech.com>
2024-10-22 15:32:35 -0400

    config2

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ const settings = {
   'clientSecret': process.env.GRAPH_CLIENT_SECRET,
   'tenantId': 'd0df057f-19eb-4ff2-8748-57e5a67e852a',
   'graphUserScopes': [
-    'user.read',
-    'mail.read',
-    'mail.send'
+    'user.read'
   ]
 };
 module.exports = {config, settings};
