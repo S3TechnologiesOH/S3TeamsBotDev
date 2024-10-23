0a74442c9258071980af68211f8cfdc5098f7f09
Christopher Atwell <catwell@mys3tech.com>
2024-10-23 14:21:57 -0400

    Update dataManager.js

--- Data/dataManager.js
+++ Data/dataManager.js
@@ -1,14 +1,22 @@
 const userEmail = undefined;
 const userDisplayName = undefined;
 const permissions = ["Ticket.Read", "Ticket.Write"];
-const userPermissions = (["catwell@mys3tech.com", "Ticket.Read"], ["jlowry@mys3tech.com", ""]);
+const userPermissions = [["catwell@mys3tech.com", "Ticket.Read"], ["jlowry@mys3tech.com", ""]];
 module.exports = {userEmail, userDisplayName, permissions, userPermissions};
 
-async function hasCommandPermission(permIndex){
-    if(userPermissions.includes(userEmail, permIndex)){
-        return true;
-    } else {
+async function hasCommandPermission(email, permIndex){
+
+    if (permIndex < 0 || permIndex >= permissions.length) {
+        console.error(`Invalid permission index: ${permIndex}`);
         return false;
-    }
+      }
+
+    const permissionToCheck = permissions[permIndex];
+
+    return userPermissions.some(
+        ([userEmail, userPermission]) =>
+          userEmail === email && userPermission === permissionToCheck
+      );
+
 }
 module.exports = { hasCommandPermission };
