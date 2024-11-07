d2c8fcf0df84a63ac5cacda88a6bfd2d76d6806b
Christopher Atwell <catwell@mys3tech.com>
2024-11-07 14:32:34 -0500

    Update connectwiseAPI.js

--- ConnectWise/connectwiseAPI.js
+++ ConnectWise/connectwiseAPI.js
@@ -30,7 +30,7 @@ try {
 
 async function testProducts(){
   try{
-    var response = await cwProductItems.procurementProductsIdGet({id: 53357});
+    var response = await cwProductItems.procurementProductsGet({conditions: "ticket/id=53357"});
     console.log("Got procurement products: ", response);
   }
   catch{
