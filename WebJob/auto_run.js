4c1235d8f2bd9aec37e702decfdb1a1b4b73f421
Christopher Atwell <catwell@mys3tech.com>
2025-02-03 13:24:17 -0500

    Update auto_run.js

--- WebJob/auto_run.js
+++ WebJob/auto_run.js
@@ -8,17 +8,18 @@ async function run() {
     const timestamp = new Date().toISOString();
     const message = `-- fetching deals -- executed at ${timestamp}\n`;
 
-    const deals = await fetchDeals(100);
+    //const deals = await fetchDeals(100);
     console.log("Deals: ", deals);
-    await processDeals(deals, false);
+    //await processDeals(deals, false);
 
     console.log(message);
 
-    fs.appendFile('log.txt', message, (err) => {
+    /*fs.appendFile('log.txt', message, (err) => {
         if (err) {
             console.error('Error writing to log file:', err);
         }
     });
+    */
 }
 
 module.exports = { run };
